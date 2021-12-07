d23f68ace798591fbb45c01e2dcd9caa4331a932
HuskyHsu <schi.hsu@gmail.com>
2021-12-07 23:54:14 +0800

    feat: 🎸 add q2

--- template/script.js
+++ template/script.js
@@ -1,16 +1,3 @@
-const answer_1_ele = document.getElementById("answer_1");
-let answer_1 = {
-  city: "STRING",
-  town: "STRING",
-  name: "STRING",
-  temp: 0,
-  location: {
-    lon: 0,
-    lat: 0,
-  },
-};
-answer_1_ele.value = JSON.stringify(answer_1, null, "    ");
-
 const getCurrentData = async () => {
   const cwbHost = "https://opendata.cwb.gov.tw";
   const apiPath = "api/v1/rest/datastore/O-A0001-001";
@@ -27,3 +14,20 @@ const getCurrentData = async () => {
 };
 
 getCurrentData();
+
+const answers = new Array(2).fill({});
+answers[0] = {
+  city: "STRING",
+  town: "STRING",
+  name: "STRING",
+  temp: 0,
+  location: {
+    lon: 0,
+    lat: 0,
+  },
+};
+
+answers.forEach((answer, index) => {
+  const answer_node = document.getElementById(`answer_${index + 1}`);
+  answer_node.value = JSON.stringify(answer, null, "    ");
+})
