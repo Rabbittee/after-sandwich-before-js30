0085044a028be91ba541c7fccd6da2fa6927f655
Erica <erica.du0916@gmail.com>
2021-12-28 01:25:47 +0800

    q3

--- Erica/js/api.js
+++ Erica/js/api.js
@@ -1,16 +1,19 @@
-export async function fetchApi(id, ...params) {
+export async function fetchApi(id, params) {
     const cwbHost = 'https://opendata.cwb.gov.tw'
     const apiPath = 'api/v1/rest/datastore'
-    const token = new URLSearchParams({
-        Authorization: 'CWB-7542F746-7741-4BCD-9D92-02BA8BA53A03'
+    const toke = 'CWB-7542F746-7741-4BCD-9D92-02BA8BA53A03'
+
+    const query = new URLSearchParams({
+        Authorization: toke,
+        ...params,
     })
-    const value = params.map((item) => item.split(' ')).join('&')
-    const response = await fetch(`${cwbHost}/${apiPath}/${id}?${token.toString()}&${value}`)
+
+    const response = await fetch(`${cwbHost}/${apiPath}/${id}?${query}`)
     return response.json()
 }
 
 
-export async function getCurrentData(id) {
-    const response = await fetchApi(id)
+export async function getCurrentData(id, ...params) {
+    const response = await fetchApi(id, params[0])
     return response.records.location
 }
---
+++ Erica/js/q4.js
@@ -0,0 +1,10 @@
+import { render, getLocation, getWeather } from './methods.js'
+const invalid = '-99'
+const rain = 'HOUR_24'
+const city = 'CITY'
+const town = 'TOWN'
+
+/** Q4 */
+export default function q4(datum) {
+    console.log(datum)
+}
