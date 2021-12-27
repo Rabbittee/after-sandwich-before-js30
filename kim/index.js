39605da5bfeadf50c90b0f338c3dfd28decdbb1e
Kimberly <yantingfen@yantingfendeMacBook-Air.local>
2021-12-28 00:02:30 +0800

    fix: 🐛 String become Number, No data fixed, const fixed, 巢狀解構 for exam one.

--- kim/index.js
+++ kim/index.js
@@ -2,10 +2,10 @@ const baseURL = "https://opendata.cwb.gov.tw/api";
 const token = "CWB-1E70461D-B346-4378-A55A-DB337F9BD7C5";
 const apiPathA = "/v1/rest/datastore/O-A0001-001";
 const apiPathB = "/v1/rest/datastore/F-D0047-063";
-let a = `${baseURL}${apiPathA}?Authorization=${token}`;
-let b = `${baseURL}${apiPathB}?Authorization=${token}`;
+const weatherData = `${baseURL}${apiPathA}?Authorization=${token}`;
+const taipeiWeek = `${baseURL}${apiPathB}?Authorization=${token}`;
 
-fetch(a)
+fetch(weatherData)
   .then(function (response) {
     return response.json();
   })
@@ -19,7 +19,7 @@ fetch(a)
     console.log(err);
   });
 
-fetch(b)
+fetch(taipeiWeek)
   .then(function (response) {
     return response.json();
   })
@@ -36,135 +36,185 @@ function pullAlldata(data) {
   return location;
 }
 
+// -99 in this weather project mean no data
+const noData = -99
+
 // Exam One
 function findTempMin(data) {
   const location = pullAlldata(data);
-  const newTempArray = [];
+  const list = []
 
-  for (i = 0; i < location.length; i++) {
-    const time = location[i].time.obsTime;
-    let city = location[i].parameter[0].parameterValue;
-    let town = location[i].parameter[2].parameterValue;
-    let locationName = location[i].locationName;
-    let temp = location[i].weatherElement[3].elementValue;
-    let lon = location[i].lon;
-    let lat = location[i].lat;
-
-    // Push
-    newTempArray.push({ time, city, town, locationName, temp, lon, lat });
-
-    // Sort
-    newTempArray.sort(function (a, b) {
-      return a.temp - b.temp;
-    });
+  for (const item of location) {
+    // Destructuring
+    const {
+      lon: lon, 
+      lat: lat, 
+      locationName: locationName,
+      time: {obsTime: time}
+      } = item;
+    const obj = { lon, lat, locationName, time };
+    
+    for (const param of item.parameter) {
+      if ( param.parameterName === 'CITY' ) {
+        const city = param.parameterValue
+        obj.city = city
+      } else if ( param.parameterName === "TOWN" ) {
+        const town = param.parameterValue
+        obj.town = town
+      }
+    }
+    for (const param of item.weatherElement) {
+      if ( param.elementName === "TEMP") {
+        const temp = param.elementValue
+        const newTemp = Number(temp)
+        if ( newTemp !==  noData) {
+          obj.newTemp = newTemp
+        }
+      }
+    }
+    list.push(obj)
   }
-  // Filter
-  let filterNewTempArray = newTempArray.filter((e) => e.temp !== "-99");
+  // sort
+  list.sort(function(a, b){
+    return a.newTemp - b.newTemp
+  })
+
   document.getElementById("examOne_answer").innerHTML = `
   <li><span>自動氣象站資料-無人自動站氣象資料</span></li>
-  <li><span>觀測資料時間</span> ${filterNewTempArray[0].time}</li>
-  <li><span>縣市</span> ${filterNewTempArray[0].city}</li>
-  <li><span>鄉鎮</span> ${filterNewTempArray[0].town}</li>
-  <li><span>測站名稱</span> ${filterNewTempArray[0].locationName}</li>
-  <li><span>溫度</span><span> ${filterNewTempArray[0].temp}</span></li>
-  <li><span>緯度</span> ${filterNewTempArray[0].lat}</li>
-  <li><span>經度</span> ${filterNewTempArray[0].lon}</li>
+  <li><span>觀測資料時間</span> ${list[0].time}</li>
+  <li><span>縣市</span> ${list[0].city}</li>
+  <li><span>鄉鎮</span> ${list[0].town}</li>
+  <li><span>測站名稱</span> ${list[0].locationName}</li>
+  <li><span>溫度</span><span> ${list[0].newTemp}</span></li>
+  <li><span>緯度</span> ${list[0].lat}</li>
+  <li><span>經度</span> ${list[0].lon}</li>
   `;
 }
 
 // Exam Two
 function findMountain(data) {
+  // Get data
   const location = pullAlldata(data);
-  const five = []; // 0 - 500
-  const one = []; // - 1000
-  const oneFive = []; // - 1500
-  const two = []; // - 2000
-  const twoFive = []; // - 2500
-  const three = []; // - 3000
-  const threeFive = []; // - 3500
-  const four = []; //8. - 4000
 
-  for (i = 0; i < location.length; i++) {
-    let elve = location[i].weatherElement[0].elementValue;
-    let temp = location[i].weatherElement[3].elementValue;
-    let city = location[i].parameter[0].parameterValue;
-    let town = location[i].parameter[2].parameterValue;
-    const newElve = Math.floor(elve / 500) * 500;
-    let obj = Object.assign({ newElve, temp, city, town });
-
-    if (obj.newElve < 500 && obj.temp !== "-99") {
-      five.push(obj);
-    } else if (obj.newElve < 1000 && obj.temp !== "-99") {
-      one.push(obj);
-    } else if (obj.newElve < 1500 && obj.temp !== "-99") {
-      oneFive.push(obj);
-    } else if (obj.newElve < 2000 && obj.temp !== "-99") {
-      two.push(obj);
-    } else if (obj.newElve < 2500 && obj.temp !== "-99") {
-      twoFive.push(obj);
-    } else if (obj.newElve < 3000 && obj.temp !== "-99") {
-      three.push(obj);
-    } else if (obj.newElve < 3500 && obj.temp !== "-99") {
-      threeFive.push(obj);
-    } else if (obj.newElve < 4000 && obj.temp !== "-99") {
-      four.push(obj);
+  // Claen data
+  const obj = {}
+  for (const item of location ){
+    for (const param of item.weatherElement) {
+      if (param.elementName === 'ELEV') {
+        const elev = Number(param.elementValue)
+        if (elev !== noData) {
+          const newElev = Math.floor(elev / 500) * 500
+          obj.newElev = newElev
+        }
+      }
     }
+    for (const param of item.parameter) {
+      if ( param.parameterName === 'CITY' ) {
+        const city = param.parameterValue
+        obj.city = city
+      } else if ( param.parameterName === "TOWN" ) {
+        const town = param.parameterValue
+        obj.town = town
+      }
+    }
+    for (const param of item.weatherElement) {
+      if ( param.elementName === "TEMP") {
+        const temp = param.elementValue
+        const newTemp = Number(temp)
+        if ( newTemp !==  noData) {
+          obj.newTemp = newTemp
+        }
+      }
+    }
+    console.log(obj)
   }
-  // sort
-  const newArray = [five, one, oneFive, two, twoFive, three, threeFive, four];
-  for (i = 0; i < newArray.length; i++) {
-    newArray[i].sort(function (a, b) {
-      Number(a.temp, b.temp);
-      return a.temp - b.temp;
-    });
-  }
-
-  document.getElementById("examTwo_answer").innerHTML = `
-  <li>500 公尺以下<span>     ${five[0].city} ${five[0].town} 溫度 ${five[0].temp}度 </span></li>
-  <li>500 - 1000公尺<span>  ${one[0].city} ${one[0].town} 溫度${one[0].temp}度 </span></li>
-  <li>1000 - 1500公尺<span> ${oneFive[0].city} ${oneFive[0].town} 溫度${oneFive[0].temp}度 </span></li>
-  <li>1500 - 2000公尺<span> ${two[0].city} ${two[0].town} 溫度${two[0].temp}度 </span></li>
-  <li>2000 - 2500公尺<span> ${twoFive[0].city} ${twoFive[0].town} 溫度${twoFive[0].temp}度 </span></li>
-  <li>2500 - 3000公尺<span> ${three[0].city} ${three[0].town} 溫度${three[0].temp}度 </span></li>
-  <li>3000 - 3500公尺<span> ${threeFive[0].city} ${threeFive[0].town} 溫度${threeFive[0].temp}度 </span></li>
-  <li>3500 公尺以上<span>${four[0].city} ${four[0].town} 溫度 ${four[0].temp}度 </span></li>
-  `;
 }
+
+  // Deal data
+  // const newArray = [five, one, oneFive, two, twoFive, three, threeFive, four];
+  // const newArray = []
+  // newArray.push(obj)
+
+  // console.log()
+  // const five = []; // 0 - 500
+  // const one = []; // - 1000
+  // const oneFive = []; // - 1500
+  // const two = []; // - 2000
+  // const twoFive = []; // - 2500
+  // const three = []; // - 3000
+  // const threeFive = []; // - 3500
+  // const four = []; //8. - 4000
+
+  // newArray.push(obj)
+  // console.log(newArray)
+
+
+  // for (i = 0; i < newArray.length; i++) {
+  //   newArray[i].sort(function (a, b) {
+  //     return a.temp - b.temp;
+  //   });
+  // }
+
+  // document.getElementById("examTwo_answer").innerHTML = `
+  // <li>500 公尺以下<span>     ${five[0].city} ${five[0].town} 溫度 ${five[0].temp}度 </span></li>
+  // <li>500 - 1000公尺<span>  ${one[0].city} ${one[0].town} 溫度${one[0].temp}度 </span></li>
+  // <li>1000 - 1500公尺<span> ${oneFive[0].city} ${oneFive[0].town} 溫度${oneFive[0].temp}度 </span></li>
+  // <li>1500 - 2000公尺<span> ${two[0].city} ${two[0].town} 溫度${two[0].temp}度 </span></li>
+  // <li>2000 - 2500公尺<span> ${twoFive[0].city} ${twoFive[0].town} 溫度${twoFive[0].temp}度 </span></li>
+  // <li>2500 - 3000公尺<span> ${three[0].city} ${three[0].town} 溫度${three[0].temp}度 </span></li>
+  // <li>3000 - 3500公尺<span> ${threeFive[0].city} ${threeFive[0].town} 溫度${threeFive[0].temp}度 </span></li>
+  // <li>3500 公尺以上<span>${four[0].city} ${four[0].town} 溫度 ${four[0].temp}度 </span></li>
+  // `;
+// }
 
 // Exam Three
 function findRainTop(data) {
+  // Get Data
   let pullAlldataArray = pullAlldata(data);
-  let array = [];
+  let list = [];
 
-  // Get the name and value become an object and put in array
-  for (i = 0; i < pullAlldataArray.length; i++) {
-    let res = pullAlldataArray[i].parameter[0];
-    let rainValue = pullAlldataArray[i].weatherElement[6];
-    let obj = Object.assign({}, res, rainValue);
-    array.push(obj);
-  }
-  // Rank
-  array.sort(function (a, b) {
-    return b.elementValue - a.elementValue;
-  });
+  // Clean data
+    for (const item of pullAlldataArray){
+      const obj = {}
+      const parameter = item.parameter
+      for (const city of parameter ){
+        if (city.parameterName === "CITY") {
+          obj.city = city
+        } 
+      }
+      const weatherElement = item.weatherElement
+      for (const rain of weatherElement) {
+        let topRain = Number(rain.elementValue)
+        if (rain.elementName === 'H_24R' && topRain !== noData) {
+          obj.topRain = topRain
+        }
+      }
+      list.push(obj)
+    }
 
-  for (var i = 0; i < 20; i++) {
-    array[i].rank = i + 1;
+    // Deal data
+    list.sort(function (a, b) {
+      return b.topRain - a.topRain;
+    });
+
+    // Render
+    for (var i = 0; i < 20; i++) {
+    list[i].rank = i + 1;
     document.getElementById("examThree_answer").innerHTML += `
     <li>
       <span class="rankList">
-        ${array[i].rank} &ensp;
+        ${list[i].rank} &ensp;
       </span>
-        ${array[i].parameterName}
-        ${array[i].parameterValue}
-        ${array[i].elementName}
+        ${list[i].city.parameterName}
+        ${list[i].city.parameterValue}
       <span class="lowTemp">
-        ${array[i].elementValue}
+        ${list[i].topRain}
       <span>
     </li>
     `;
   }
 }
+
 
 // Exam Four
 function expectFutureMaxT(data) {
