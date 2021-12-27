9927e1394bd64c51345af5483bda8e6c98126545
Alpha <alphatero@icloud.com>
2021-12-27 09:12:56 +0800

    feat: 🎸 test to update a new folder with reactjs

---
+++ alpha/reactjs/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <main className="w-screen max-w-3xl flex flex-col mx-auto my-16 space-y-6 px-4">
+      <h4 class="text-2xl font-black">JS讀書會後測啦</h4>
+      <section className="bg-white text-black text-opacity-70 rounded-2xl shadow-xl p-8"></section>
+    </main>
+  );
+}
+
+export default App;
