2c182cf944b0c7b5c7f7c1da23d74e4061829582
haha861924 <qq861924@gmail.com>
2021-12-28 11:50:55 +0800

    style: add tailwind style

--- haha/tailwind.config.js
+++ haha/tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    100: '#e5e7eb',
+                    200: '#f1f5f9',
+                    300: '#F8FAFC',
+                    600: '#94A3B8',
+                },
+                red: {
+                    500: '#F43F5E',
+                },
+            },
+        },
+    },
+    plugins: [],
 }
