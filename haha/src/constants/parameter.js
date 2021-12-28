50670d1d408ff701fe559e4585a7e74213724b5d
haha861924 <qq861924@gmail.com>
2021-12-28 12:28:57 +0800

    refactor: utils ans constants

--- haha/src/constants/parameter.js
+++ haha/src/constants/parameter.js
@@ -1,22 +1,9 @@
-import dayjs from 'dayjs'
+const host = import.meta.env.VITE_BACKEND_HOST
 
-const token = 'CWB-3DDB77B5-643B-4E2D-B0AE-4218ADEB7C9F'
-const host = 'https://opendata.cwb.gov.tw/api/v1/rest/datastore/'
-const paramsObj = {
-    Headers: {
-        Authorization: token,
-    },
+const token = import.meta.env.VITE_BACKEND_TOKEN
+
+const header = {
+    Authorization: token,
 }
-const lastOneDay = dayjs()
-    .add(1, 'day')
-    .hour(0)
-    .minute(0)
-    .second(0)
-    .format('YYYY-MM-DDTHH:mm:ss')
-const lastTwoDay = dayjs()
-    .add(2, 'day')
-    .hour(23)
-    .minute(59)
-    .second(59)
-    .format('YYYY-MM-DDTHH:mm:ss')
-export { token, host, paramsObj, lastOneDay, lastTwoDay }
+
+export { host, header }
