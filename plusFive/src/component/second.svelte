6887e89b47e7040caa2caa197c57d959dfac3beb
Claire Li <claire.li@singularinfinity.com.tw>
2021-12-28 18:36:32 +0800

    Q2 v2

--- plusFive/src/component/second.svelte
+++ plusFive/src/component/second.svelte
@@ -5,14 +5,6 @@
   let location
   let currentTN
   let under = [[], [], [], [], [], [], [], []]
-  let under500 = []
-  let under1000 = []
-  let under1500 = []
-  let under2000 = []
-  let under2500 = []
-  let under3000 = []
-  let under3500 = []
-  let under4000 = []
   let elevTN = []
 
   onMount(async () => {
@@ -33,56 +25,30 @@
     if (response.statusText === 'OK') {
       data = await response.json()
       location = await data.records.location
-      // console.log(location)
+
       classifyElev()
     }
   }
 
   function classifyElev() {
     for(let i = 0; i < location.length; i++) {
-      // 這裡要先 filter 掉 -99 的
       const elev = location[i].weatherElement[0].elementValue
+      // 這裡要先 filter 掉 -99 的
+      if (Number(elev) === -99) continue
+
       let j = Math.floor(Number(elev) / 500)
       under[j].push(location[i])
-      // if (Number(elev) < 500) {
-      //   // under[0].push(location[i])
-      //   under500.push(location[i])
-      // } else if (Number(elev) >= 500 && Number(elev) < 1000) {
-      //   under1000.push(location[i])
-      //   // console.log(under1000)
-      // } else if (Number(elev) >= 1000 && Number(elev) < 1500) {
-      //   under1500.push(location[i])
-      // } else if (Number(elev) >= 1500 && Number(elev) < 2000) {
-      //   under2000.push(location[i])
-      // } else if (Number(elev) >=2000 && Number(elev) < 2500) {
-      //   under2500.push(location[i])
-      // } else if (Number(elev) >=2500 && Number(elev) < 3000) {
-      //   under3000.push(location[i])
-      // } else if (Number(elev) >=3000 && Number(elev) < 3500) {
-      //   under3500.push(location[i])
-      // } else if (Number(elev) >=3500 && Number(elev) < 4000) {
-      //   under4000.push(location[i])
-      // }
     }
-    console.log(under)
-    // invokeGetCurrentTN()
+
+    invokeGetCurrentTN()
   }
 
   function invokeGetCurrentTN() {
-    getCurrentTN(under500)
-    getCurrentTN(under1000)
-    getCurrentTN(under1500)
-    getCurrentTN(under2000)
-    getCurrentTN(under2500)
-    getCurrentTN(under3000)
-    getCurrentTN(under3500)
-    getCurrentTN(under4000)
-    // for (i = 0; i < under.length; i++) {
-    //   getCurrentTN(under[i])
-    // }
+    for (let i = 0; i < under.length; i++) {
+      getCurrentTN(under[i])
+    }
   }
 
-  // 參數會是 double array
   function getCurrentTN(locationArr) {
     let smallest = Number(locationArr[0].weatherElement[3].elementValue)
     let smallestIndex = 0
@@ -96,25 +62,24 @@
 
     currentTN = locationArr[smallestIndex]
     elevTN.push(currentTN)
-    console.log(elevTN)
+    elevTN = elevTN
   }
-
-  // let lowest.fill(100, 8);
-  // let result.fill("", 8)
-
-  // for(let i=0; i<array_data.length; i++){
-  //     // filter
-  //     if(temperture ==-99)  continue;
-  //     if(N <= 100)  continue;
-      
-  //     let j = hight/500;
-  //     if(lowest[j] > temperture){
-  //         lowest[j] = temperture;
-  //         result[j] = array_data[i];
-  //     }
-  // }
 </script>
 
-<div>
-  {JSON.stringify(elevTN)}
-</div>
+<h6 class="mb-3">第二題：</h6>
+{#each elevTN as elev}
+  <div class="p-1 border border-gray-200">
+    <div>
+      測站名稱：{elev.locationName}
+    </div>
+    <div>
+      溫度：{elev.weatherElement[3].elementValue}
+    </div>
+  </div>
+{/each}
+<!-- <div>
+  // 這裡會出錯，待研究
+  {elevTN[0]}
+  {elevTN[0].weatherElement[0].elementValue}
+  {JSON.stringify(elevTN[1].lat)}
+</div> -->
