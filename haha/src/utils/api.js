d5f245423c9267ec6c1d0ca22bd0188818b1d33f
haha861924 <qq861924@gmail.com>
2021-12-28 11:49:41 +0800

    feat: delete trash

--- haha/src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import { token, host, paramsObj } from '../constants/parameter.js'
-
-export default async function (url, parameter) {
-    return await fetch(
-        `${host}${url}?Authorization=${token}${parameter}`,
-        paramsObj
-    ).then((res) => res.json())
-}
