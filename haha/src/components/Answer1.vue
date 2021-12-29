ce9efe014db5f5b1c57b98197bffb2b035a65ad2
haha861924 <qq861924@gmail.com>
2021-12-29 18:35:08 +0800

    style: fix shit

--- haha/src/components/Answer1.vue
+++ haha/src/components/Answer1.vue
@@ -7,6 +7,7 @@ import { validWeather, min, formatData } from '../utils/methods.js'
 const state = reactive({
     data: {},
 })
+
 fetchData('O-A0001-001').then((res) => {
     const data = min(validWeather(res.records.location), 'TEMP')
     const { city, town, temp } = formatData(data)
