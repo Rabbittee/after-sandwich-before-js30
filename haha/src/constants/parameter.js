32db90451255eb92c57a3efcca2f6ffbde74a1ad
haha861924 <qq861924@gmail.com>
2021-12-29 18:35:35 +0800

    feat: add constants

--- haha/src/constants/parameter.js
+++ haha/src/constants/parameter.js
@@ -1,3 +1,5 @@
+import dayjs from 'dayjs'
+
 const host = import.meta.env.VITE_BACKEND_HOST
 
 const token = import.meta.env.VITE_BACKEND_TOKEN
@@ -6,4 +8,18 @@ const header = {
     Authorization: token,
 }
 
-export { host, header }
+const featureOneDay = dayjs()
+    .add(1, 'day')
+    .hour(0)
+    .minute(0)
+    .second(0)
+    .format('YYYY-MM-DDTHH:mm:ss')
+
+const featureNextDay = dayjs()
+    .add(2, 'day')
+    .hour(23)
+    .minute(59)
+    .second(59)
+    .format('YYYY-MM-DDTHH:mm:ss')
+
+export { host, header, featureOneDay, featureNextDay }
