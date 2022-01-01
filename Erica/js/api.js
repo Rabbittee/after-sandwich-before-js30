46228660c52abd98d2c0d1465117f3b98df1107b
Erica <erica.du0916@gmail.com>
2022-01-02 00:06:50 +0800

    q3 reduce answer

--- Erica/js/api.js
+++ Erica/js/api.js
@@ -1,11 +1,11 @@
 export async function fetchApi(id, params) {
     const cwbHost = 'https://opendata.cwb.gov.tw'
     const apiPath = 'api/v1/rest/datastore'
-    const toke = 'CWB-7542F746-7741-4BCD-9D92-02BA8BA53A03'
+    const token = 'CWB-7542F746-7741-4BCD-9D92-02BA8BA53A03'
 
     const query = new URLSearchParams({
-        Authorization: toke,
-        ...params,
+        Authorization: token,
+        params,
     })
 
     const response = await fetch(`${cwbHost}/${apiPath}/${id}?${query}`)
@@ -13,7 +13,7 @@ export async function fetchApi(id, params) {
 }
 
 
-export async function getCurrentData(id, ...params) {
-    const response = await fetchApi(id, params[0])
+export async function getCurrentData(id, params) {
+    const response = await fetchApi(id, params)
     return response.records.location
 }
--- Erica/js/index.js
+++ Erica/js/index.js
@@ -11,7 +11,7 @@ import q3 from './q3.js';
 /** Set up scripts */
 async function setup() {
     toggleTabs()
-    q1(await getCurrentData('O-A0001-001'))
+    q1(await getCurrentData('O-A0001-001', {elementName: 'TEMP'}))
     q2(await getCurrentData('O-A0001-001', {elementName: 'TEMP,ELEV'}))
     q3(await getCurrentData('O-A0002-001', {elementName: 'HOUR_24'}))
     // q4(await getCurrentData('F-D0047-089', {locationName: '新竹縣'}))
