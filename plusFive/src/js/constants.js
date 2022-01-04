e43f6a0badb82ccf98080ec501ea12f163b3cdeb
Claire Li <claire.li@singularinfinity.com.tw>
2022-01-04 11:38:20 +0800

    新增共用常數

---
+++ plusFive/src/js/constants.js
@@ -0,0 +1,11 @@
+const cwbHost = 'https://opendata.cwb.gov.tw'
+
+const TempApiPath = 'api/v1/rest/datastore/O-A0001-001'
+const HOUR_24ApiPath = 'api/v1/rest/datastore/O-A0002-001'
+const NextTwoDayApiPath = 'api/v1/rest/datastore/F-D0047-089'
+
+const token = 'CWB-34AC2BFB-2272-41E3-84FE-AAF40C0C42AA'
+const HOUR_24 = 'HOUR_24'
+const CITY = 'CITY'
+
+export { cwbHost, TempApiPath, HOUR_24ApiPath, NextTwoDayApiPath, token, HOUR_24, CITY }
